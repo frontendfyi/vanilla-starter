93b9fed6f2a7804704a2bb0da0cabb7880514ca7
Jeroen Reumkens <hello@jeroenreumkens.nl>
2020-02-23 14:44:11 +0100

    Added comments to js and ts files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log('JavaScript it is working');
+// This JavaScript file is imported in index.html and automatically transpiled by Parcel.
+// This is only here as an example to demonstrate that Parcel is doing what it does best.
+console.log('JavaScript is working');
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
+// This TypeScript file is imported in index.html and automatically transpiled by Parcel.
+// This is only here as an example to demonstrate that Parcel is doing what it does best.
 const foo: string = 'TypeScript';
 console.log(`${foo} is also working`);
